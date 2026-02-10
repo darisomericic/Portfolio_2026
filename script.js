c107f9826d0ae8c5da37da24d71f6b2743b5c399
Daris Omericic <darisomericicpg@gmail.com>
2026-02-10 12:28:49 +0100

    lil fix + prettier code

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 // 1. Select all links inside your nav menu
 const navLinks = document.querySelectorAll("#nav-menu a");
 
 // 2. Loop through each link
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        // 3. Remove the 'active' class to hide the menu
-        navMenu.classList.remove("active");
-    });
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    // 3. Remove the 'active' class to hide the menu
+    navMenu.classList.remove("active");
+  });
 });
 
-document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
